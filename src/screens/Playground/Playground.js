a881a2d2f6acb33a189daa609e706fc9240d9e07
TrinhVanCanh <canhtv@rabiloo.com>
2022-08-16 16:09:15 +0700

    playground animation

--- src/screens/Playground/Playground.js
+++ src/screens/Playground/Playground.js
@@ -1,33 +1,18 @@
-import React, {useEffect, useRef, useState} from 'react';
-import {
-  ActivityIndicator,
-  Image,
-  StyleSheet,
-  Text,
-  View,
-  Alert,
-} from 'react-native';
-import FastImage from 'react-native-fast-image';
-import {
-  AppAsyncImage,
-  AppButtonNormal,
-  AppContainer,
-  AppIcon,
-  AppInputImageArray,
-  AppInputImageAvatar,
-  AppViewLoading,
-  VectorIcon,
-} from '../../commons';
-import {FetchApi, getResourceImage, Sizes, useAppContext} from '../../utils';
-import {useForm} from 'react-hook-form';
-import {Modal} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, View, Alert, Button} from 'react-native';
+import {AppButtonNormal, AppContainer} from '../../commons';
+import {FetchApi, Sizes, useAppContext} from '../../utils';
+
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
 
 const _space = <View style={{height: Sizes.padding}} />;
 
 export function Playground() {
-  const {Styles, Colors} = useAppContext();
-  const [state, setState] = useState(false);
-  const {control, handleSubmit} = useForm();
+  const {Styles} = useAppContext();
 
   const onPress = async () => {
     const result = await FetchApi.getVideos();
@@ -37,15 +22,22 @@ export function Playground() {
       Alert.alert(result.message);
     }
   };
+
+  const height = useSharedValue(50);
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      height: height.value,
+    };
+  });
+  console.log('rerender playground');
   return (
     <AppContainer style={{padding: Sizes.padding * 2}}>
-      {/* <AppInputImageArray control={control} name={'avatar'} /> */}
+      <Animated.View style={[{backgroundColor: 'red'}, animatedStyle]} />
       {_space}
-      <AppButtonNormal
-        label={'Submit'}
-        containerStyle={Styles.solidButtonContainer}
-        // onPress={handleSubmit(data => console.log('form', data))}
-        onPress={onPress}
+      <Button
+        title="Animating"
+        onPress={() => (height.value = withTiming(Math.random() * 300))}
       />
       {_space}
     </AppContainer>
