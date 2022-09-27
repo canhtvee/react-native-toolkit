cd871b7a296690da01725555ed6412494c6f8809
TrinhVanCanh <canhtv@rabiloo.com>
2022-09-27 21:22:16 +0700

    [New] config module resolve

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,19 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['react-native-reanimated/plugin'],
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@screens': './src/screens',
+          '@commons': './src/commons',
+          '@utils': './src/utils',
+          '@container': './src/container',
+        },
+      },
+    ],
+  ],
 };
