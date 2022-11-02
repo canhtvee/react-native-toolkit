9e076190c8320648b9f54a6d78baf58b89254745
TrinhVanCanh <canhtv@rabiloo.com>
2022-11-02 13:56:54 +0700

    [Update] config environment

--- src/utils/resources/system.ts
+++ src/utils/resources/system.ts
@@ -8,27 +8,28 @@ const appVersion = Platform.select({ios: '0.0.1', android: '0.0.1'});
  * config information of App
  */
 
-const host = {
-  api: 'http://api.com',
-}; //stg
-
 // const host = {
-//   api: 'https://api.app.com',
-// }; //pro
+//   api: 'http://api.com',
+// }; //stg
 
-//stg
-const appKeys = {
-  codePush: Platform.select({
-    ios: '0Flb9VF_ReYEFMaCRKL2HLsd_cfwmN4XQE4E_',
-    android: 'N1orohLsv5oTtuuW_sbML90rdQQqJry5edTTj',
-  }),
-};
-// pro
+const host = {
+  api: 'https://api.app.com',
+}; //pro
+
+// //stg
 // const appKeys = {
 //   codePush: Platform.select({
-//     ios: 'QWyGnBfz2KLiqua0QK7VHsEFpoEFHZyahtJlO',
-//     android: '5Xu-qtJ9OT88tQC8FWEkOx0_BIGBB64yvf_dr',
+//     ios: '0Flb9VF_ReYEFMaCRKL2HLsd_cfwmN4XQE4E_',
+//     android: 'N1orohLsv5oTtuuW_sbML90rdQQqJry5edTTj',
 //   }),
 // };
+
+// pro
+const appKeys = {
+  codePush: Platform.select({
+    ios: 'QWyGnBfz2KLiqua0QK7VHsEFpoEFHZyahtJlO',
+    android: '5Xu-qtJ9OT88tQC8FWEkOx0_BIGBB64yvf_dr',
+  }),
+};
 
 export {host, isIOS, deviceType, appVersion, appKeys};
